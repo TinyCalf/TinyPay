c6481d8cd62e5f6d812b3d64202336ec56c93a69
Jonathan <jonathan@localhost.localdomain>
2017-11-10 14:18:39 +0800

    完成消息队列并完成基本测试

--- BitcoinSeries/zmqtestClient.js
+++ BitcoinSeries/zmqtestClient.js
@@ -4,7 +4,6 @@ const sock = zmq.socket('pull')
 sock.connect("tcp://127.0.0.1:1999");
 console.log('Worker connected to port 1999')
 
-sock.on('message', (top, msg) => {
-    console.log(top.toString());
+sock.on('message', (msg) => {
     console.log(msg.toString());
 });
--- Zeromq/zmqServer.js
+++ Zeromq/zmqServer.js
@@ -11,7 +11,7 @@ log.info('Zmq server has bound on port 1999')
 */
 exports.sendReceivedTxs = (msg) => {
   return new Promise ( (resolve, reject) => {
-    sock.send(['receivedTxs',msg]);
+    sock.send( JSON.stringify(msg) );
     log.info("zmq sent msg: ")
     log.info(msg)
     resolve();
