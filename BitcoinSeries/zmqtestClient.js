904654746ce44c3e04295484a9b2aeb7f6c8ff8c
Jonathan <jonathan@localhost.localdomain>
2017-11-13 09:51:34 +0800

    删除多余文件

--- BitcoinSeries/zmqtestClient.js
+++
@@ -1,9 +0,0 @@
-const zmq = require('zmq')
-const sock = zmq.socket('pull')
-
-sock.connect("tcp://120.92.91.36:1999");
-console.log('Worker connected to port 1999')
-
-sock.on('message', (msg) => {
-    console.log(msg.toString());
-});
