240faff2e6a6e2a3eb0a417f15cf4274f046a7f9
Jonathan <jonathan@localhost.localdomain>
2017-12-10 09:41:53 +0800

    修改config

--- config.js
+++ config.js
@@ -8,32 +8,32 @@ module.exports = {
   },
   currencies:{
     /****************************比特币系列**************************************/
-    btc:{
-      category:'bitcoin',
-    	protocol:"http",
-    	host:'127.0.0.1',
-    	port:'8332',
-    	user:'ebo',
-    	pass:'ebo123',
-      txCheckDuration: 5 * 60 * 1000,// 5min
-      incomeLimit:0.001, // 最小充值提现下限
-      outcomeLimit:1, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
-      env:'prod',
-    },
-    // bcc:{
+    // btc:{
     //   category:'bitcoin',
     // 	protocol:"http",
     // 	host:'127.0.0.1',
-    // 	port:'10081',
+    // 	port:'8332',
     // 	user:'ebo',
     // 	pass:'ebo123',
-    //   txCheckDuration: 2 * 60 * 1000,// 2min
-    //   incomeLimit:0.01, // 最小充值提现下限
-    //   outcomeLimit:10, // 最大提币上限
+    //   txCheckDuration: 5 * 60 * 1000,// 5min
+    //   incomeLimit:0.001, // 最小充值提现下限
+    //   outcomeLimit:1, // 最大提币上限
     //   confirmationsLimit:6, // 交易确认数
     //   env:'prod',
     // },
+    bcc:{
+      category:'bitcoin',
+    	protocol:"http",
+    	host:'127.0.0.1',
+    	port:'10081',
+    	user:'ebo',
+    	pass:'ebo123',
+      txCheckDuration: 0.5 * 60 * 1000,// 2min
+      incomeLimit:0.01, // 最小充值提现下限
+      outcomeLimit:10, // 最大提币上限
+      confirmationsLimit:6, // 交易确认数
+      env:'prod',
+    },
     // ltc:{
     //   category:'bitcoin',
     // 	protocol:"http",
@@ -81,7 +81,7 @@ module.exports = {
     	port:'8330',
     	user:'ebo',
     	pass:'ebo123',
-      txCheckDuration: 1000,// 1min
+      txCheckDuration: 0.5 * 60 * 1000,// 30s
       incomeLimit:1, // 最小充值提现下限
       outcomeLimit:10000, // 最大提币上限
       confirmationsLimit:6, // 交易确认数
@@ -101,17 +101,17 @@ module.exports = {
     //   confirmationsLimit:6, // 交易确认数
     //   env:'prod',
     // },
-    eth:{
-      category:'ethereum',
-    	protocol:"http",
-    	host:'127.0.0.1',
-    	port:'10070',
-      txCheckDuration: 1 * 60 * 1000,//  1min
-      incomeLimit:1, // 最小充值下限
-      outcomeLimit:100, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
-      env:'prod',
-    },
+    // eth:{
+    //   category:'ethereum',
+    // 	protocol:"http",
+    // 	host:'127.0.0.1',
+    // 	port:'10070',
+    //   txCheckDuration: 1 * 60 * 1000,//  1min
+    //   incomeLimit:1, // 最小充值下限
+    //   outcomeLimit:100, // 最大提币上限
+    //   confirmationsLimit:6, // 交易确认数
+    //   env:'prod',
+    // },
     /**************************************************************************/
   }
 }
