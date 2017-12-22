f186867b443a7663c5e2ddd9ae26448e9414231c
Jonathan <jonathan@localhost.localdomain>
2017-12-22 08:20:57 +0800

    修改错误

--- Database/Models/Income.model.js
+++ Database/Models/Income.model.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 var IncomeSchema = new mongoose.Schema({
   name:            {type:String, required:true},
-  txid:            {type:String, required:true， unique:true},
+  txid:            {type:String, required:true, unique:true},
   to:              {type:String},
   from:            {type:String, required:true},
   amount:          {type:Number, required:true},
--- config.js
+++ config.js
@@ -47,32 +47,32 @@ module.exports = {
       confirmationsLimit:6, // 交易确认数
       env:'prod',
     },
-    utc:{
-      category:'bitcoin',
-    	protocol:"http",
-    	host:'127.0.0.1',
-    	port:'1995',
-    	user:'ebo',
-    	pass:'ebo123',
-      txCheckDuration: 1 * 60 * 1000,// 1min
-      incomeLimit:1, // 最小充值提现下限
-      outcomeLimit:10000, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
-      env:'prod',
-    },
-    tch:{
-      category:'bitcoin',
-    	protocol:"http",
-    	host:'127.0.0.1',
-    	port:'10010',
-    	user:'ebo',
-    	pass:'ebo123',
-      txCheckDuration: 1 * 60 * 1000,// 1min
-      incomeLimit:1, // 最小充值提现下限
-      outcomeLimit:10000, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
-      env:'prod',
-    },
+    // utc:{
+    //   category:'bitcoin',
+    // 	protocol:"http",
+    // 	host:'127.0.0.1',
+    // 	port:'1995',
+    // 	user:'ebo',
+    // 	pass:'ebo123',
+    //   txCheckDuration: 1 * 60 * 1000,// 1min
+    //   incomeLimit:1, // 最小充值提现下限
+    //   outcomeLimit:10000, // 最大提币上限
+    //   confirmationsLimit:6, // 交易确认数
+    //   env:'prod',
+    // },
+    // tch:{
+    //   category:'bitcoin',
+    // 	protocol:"http",
+    // 	host:'127.0.0.1',
+    // 	port:'10010',
+    // 	user:'ebo',
+    // 	pass:'ebo123',
+    //   txCheckDuration: 10 * 1000,// 10s
+    //   incomeLimit:1, // 最小充值提现下限
+    //   outcomeLimit:10000, // 最大提币上限
+    //   confirmationsLimit:6, // 交易确认数
+    //   env:'prod',
+    // },
     /****************************比特币系列 私有链********************************/
     // rbtc:{
     //   category:'bitcoin',
@@ -90,26 +90,26 @@ module.exports = {
 
     /****************************以太坊系列**************************************/
     //TODO： 主钱包地址为数据库第一条，之后需要全部删除
-    // etc:{
-    //   category:'ethereum',
-    // 	protocol:"http",
-    // 	host:'127.0.0.1',
-    // 	port:'10060',
-    //   txCheckDuration: 1 * 60 * 1000,// 1min
-    //   incomeLimit:1, // 最小充值提现下限
-    //   outcomeLimit:100, // 最大提币上限
-    //   confirmationsLimit:6, // 交易确认数
-    //   env:'prod',
-    // },
+    etc:{
+      category:'ethereum',
+    	protocol:"http",
+    	host:'127.0.0.1',
+    	port:'10060',
+      txCheckDuration: 20 * 1000,// 20s
+      incomeLimit:1, // 最小充值提现下限
+      outcomeLimit:100, // 最大提币上限
+      confirmationsLimit:10, // 交易确认数
+      env:'prod',
+    },
     eth:{
       category:'ethereum',
     	protocol:"http",
     	host:'127.0.0.1',
     	port:'10070',
-      txCheckDuration: 1 * 60 * 1000,//  1min
+      txCheckDuration: 20 * 1000,// 20s
       incomeLimit:1, // 最小充值下限
       outcomeLimit:100, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
+      confirmationsLimit:10, // 交易确认数
       env:'prod',
     },
     /**************************************************************************/
