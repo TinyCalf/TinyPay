cf1fb6bf9b10ae26ec5447cbba8b9faa4fb2f0af
Jonathan <jonathan@localhost.localdomain>
2017-12-25 00:27:44 +0800

    修改config文件

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
     	port:'8332',
     	user:'ebo',
     	pass:'ebo123',
-      txCheckDuration: 5 * 60 * 1000,// 5min
-      incomeLimit:0.001, // 最小充值提现下限
+      txCheckDuration: 1 * 60 * 1000,// 1min
+      incomeLimit:0.00001, // 最小充值提现下限
       outcomeLimit:10, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
+      confirmationsLimit:10, // 交易确认数
       env:'prod',
     },
     bcc:{
@@ -28,10 +28,10 @@ module.exports = {
     	port:'10081',
     	user:'ebo',
     	pass:'ebo123',
-      txCheckDuration: 0.5 * 60 * 1000,// 2min
-      incomeLimit:0.01, // 最小充值提现下限
+      txCheckDuration: 0.5 * 60 * 1000,// 30s
+      incomeLimit:0.00001, // 最小充值提现下限
       outcomeLimit:100, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
+      confirmationsLimit:10, // 交易确认数
       env:'prod',
     },
     ltc:{
@@ -41,10 +41,10 @@ module.exports = {
     	port:'10000',
     	user:'ebo',
     	pass:'ebo123',
-      txCheckDuration: 2 * 60 * 1000,// 2min
-      incomeLimit:0.01, // 最小充值提现下限
+      txCheckDuration: 30 * 1000,// 30s
+      incomeLimit:0.00001, // 最小充值提现下限
       outcomeLimit:100, // 最大提币上限
-      confirmationsLimit:6, // 交易确认数
+      confirmationsLimit:10, // 交易确认数
       env:'prod',
     },
     // utc:{
@@ -60,33 +60,33 @@ module.exports = {
     //   confirmationsLimit:6, // 交易确认数
     //   env:'prod',
     // },
-    // tch:{
-    //   category:'bitcoin',
-    // 	protocol:"http",
-    // 	host:'127.0.0.1',
-    // 	port:'10010',
-    // 	user:'ebo',
-    // 	pass:'ebo123',
-    //   txCheckDuration: 10 * 1000,// 10s
-    //   incomeLimit:1, // 最小充值提现下限
-    //   outcomeLimit:10000, // 最大提币上限
-    //   confirmationsLimit:6, // 交易确认数
-    //   env:'prod',
-    // },
+    tch:{
+      category:'bitcoin',
+    	protocol:"http",
+    	host:'127.0.0.1',
+    	port:'10010',
+    	user:'ebo',
+    	pass:'ebo123',
+      txCheckDuration: 10 * 1000,// 10s
+      incomeLimit:0.00001, // 最小充值提现下限
+      outcomeLimit:10000, // 最大提币上限
+      confirmationsLimit:10, // 交易确认数
+      env:'prod',
+    },
     /****************************比特币系列 私有链********************************/
-    // rbtc:{
-    //   category:'bitcoin',
-    // 	protocol:"http",
-    // 	host:'127.0.0.1',
-    // 	port:'8330',
-    // 	user:'ebo',
-    // 	pass:'ebo123',
-    //   txCheckDuration: 0.5 * 60 * 1000,// 30s
-    //   incomeLimit:1, // 最小充值提现下限
-    //   outcomeLimit:10000, // 最大提币上限
-    //   confirmationsLimit:10, // 交易确认数
-    //   env:'prod',
-    // },
+    rbtc:{
+      category:'bitcoin',
+    	protocol:"http",
+    	host:'127.0.0.1',
+    	port:'8330',
+    	user:'ebo',
+    	pass:'ebo123',
+      txCheckDuration: 0.5 * 60 * 1000,// 30s
+      incomeLimit:0.00001, // 最小充值提现下限JonQzjnN7LFX6KKaBkyBq46K8ac3q9NocV
+      outcomeLimit:10000, // 最大提币上限
+      confirmationsLimit:10, // 交易确认数
+      env:'prod',
+    },
 
     /****************************以太坊系列**************************************/
     //TODO： 主钱包地址为数据库第一条，之后需要全部删除
@@ -96,7 +96,7 @@ module.exports = {
     	host:'127.0.0.1',
     	port:'10060',
       txCheckDuration: 20 * 1000,// 20s
-      incomeLimit:1, // 最小充值提现下限
+      incomeLimit:0.00001, // 最小充值提现下限
       outcomeLimit:100, // 最大提币上限
       confirmationsLimit:10, // 交易确认数
       env:'prod',
@@ -107,7 +107,7 @@ module.exports = {
     	host:'127.0.0.1',
     	port:'10070',
       txCheckDuration: 20 * 1000,// 20s
-      incomeLimit:1, // 最小充值下限
+      incomeLimit:0.00001, // 最小充值下限
       outcomeLimit:100, // 最大提币上限
       confirmationsLimit:10, // 交易确认数
       env:'prod',
