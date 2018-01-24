2b9424f5b0dd18f774c48e8a89d2ad7e0eff7e2f
Luo Yong <luoyong@ebo>
2018-01-24 20:53:53 +0800

    ZeroMQ server listening to multiple ports.

--- Zeromq/zmqServer.js
+++ Zeromq/zmqServer.js
@@ -1,17 +1,24 @@
 const zmq = require('zmq')
-const sock = zmq.socket('push')
+//const sock = zmq.socket('push')
 
-sock.bindSync('tcp://0.0.0.0:1999')
+var ports = [1999, 2999]
 
+var socks = new Array()
+ports.forEach((p) => {
+  socks.push(zmq.socket('push').bindSync('tcp://0.0.0.0:'+String(p)))
+})
 
 var log = require('../Logs/log.js')("ZMQ")
-log.info('Zmq server has bound on port 1999')
+log.info('Zmq server has bound on ports ' + ports)
+
 /*
 发送接受到的交易信息
 */
 exports.sendReceivedTxs = (msg) => {
   return new Promise ( (resolve, reject) => {
-    sock.send( JSON.stringify(msg) );
+    socks.forEach((sock) => {
+      sock.send( JSON.stringify(msg) );
+    })
     log.info("zmq sent msg: " + JSON.stringify(msg))
     resolve();
   })
