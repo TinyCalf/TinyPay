872b4c506118c1a78023e03cfa020ee462ee5ef8
Jonathan <839560084@qq.com>
2018-04-04 10:35:16 +0800

    修改

---
+++ Config/index.js
@@ -0,0 +1,10 @@
+var yaml = require("js-yaml");
+var fs = require("fs");
+var path = require("path")
+var load = (name)=>{
+  var ymlpath = __dirname + '/' + name+".yml"
+  console.log(ymlpath)
+  return yaml.load(fs.readFileSync(ymlpath));
+}
+var main = load("main")
+module.exports = load(main.use)
---
+++ coolpay.js
@@ -0,0 +1,38 @@
+console.log(
+'   ______            ______             \n' +
+'  / ____/___  ____  / / __ \\____ ___  __\n' +
+' / /   / __ \\/ __ \\/ / /_/ / __ `/ / / /\n' +
+'/ /___/ /_/ / /_/ / / ____/ /_/ / /_/ / \n' +
+'\\____/\\____/\\____/_/_/    \\__,_/\\__, /  \n' +
+'                               /____/ \n'+
+'数字货币支付方案 -- tiny-calf.com\n')
+
+// var BitcoinSeriesTxsDealer = require('./BitcoinSeries/TransactionDealer.js')
+// var EthereumSeriesTxsDealer = require('./EthereumSeries/TransactionDealer.js')
+// // var ERC20TxsDealer = require('./EthereumSeries/ERC20TransactionDealer.js')
+// var log = require('./Logs/log.js')('index')
+
+
+
+// /*
+// 启动比特币系列消息队列
+// */
+// BitcoinSeriesTxsDealer.start()
+// .catch ( (err)=>log.err(err) )
+//
+// /*
+// 启动以太放系列消息队列
+// */
+// EthereumSeriesTxsDealer.start()
+// .catch ( (err)=>log.err(err) )
+//
+// /*
+// 启动ERC20系列消息队列
+// */
+// //ERC20TxsDealer.start()
+// //.catch ( (err)=>log.err(err) )
+//
+// /*
+// 启动 api v1
+// */
+// var apiv1 = require("./api/v1")
