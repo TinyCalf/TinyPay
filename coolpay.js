fef178d78ad55ab27c4a39edba0d81b6ccd4388c
Jonathan <839560084@qq.com>
2018-04-25 17:49:09 +0800

    delete backup

--- coolpay.js
+++ coolpay.js
@@ -7,32 +7,4 @@ console.log(
 '                               /____/ \n'+
 '数字货币支付方案 -- tiny-calf.com\n')
 
-// var BitcoinSeriesTxsDealer = require('./BitcoinSeries/TransactionDealer.js')
-// var EthereumSeriesTxsDealer = require('./EthereumSeries/TransactionDealer.js')
-// // var ERC20TxsDealer = require('./EthereumSeries/ERC20TransactionDealer.js')
-// var log = require('./Logs/log.js')('index')
-
-
-
-// /*
-// 启动比特币系列消息队列
-// */
-// BitcoinSeriesTxsDealer.start()
-// .catch ( (err)=>log.err(err) )
-//
-// /*
-// 启动以太放系列消息队列
-// */
-// EthereumSeriesTxsDealer.start()
-// .catch ( (err)=>log.err(err) )
-//
-// /*
-// 启动ERC20系列消息队列
-// */
-// //ERC20TxsDealer.start()
-// //.catch ( (err)=>log.err(err) )
-//
-// /*
-// 启动 api v1
-// */
-// var apiv1 = require("./api/v1")
+var Ethereum = require("./Ethereum")
