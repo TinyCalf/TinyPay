6f6949d620fb3e20aa2e31d6ad02df4bb5038c4f
Jonathan <839560084@qq.com>
2018-05-16 20:42:23 +0800

    删除多余文件

--- docs-backup/test-backup/a.js
+++
@@ -1,8 +0,0 @@
-var chalk = require("chalk")
-var string = "\n---------------------------------------------------------------------\n"
-   string += "|                                                                   |\n"
-   string += "| Run 'npm run repo' to see detailed test reporter in browser !     |\n"
-   string += "| Make sure you are running this test in regtest coinnodes!         |\n"
-   string += "|                                                                   |\n"
-   string += "---------------------------------------------------------------------\n"
-console.log(chalk.blueBright(string))
